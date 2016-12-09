c8a9b8a8e119f8d05c29122d0e9eb8ebef718072
FelicityPictures <doodlefelicity@gmail.com>
2016-12-08 23:39:44 -0500

    edge cases. Buggy

--- 11-2.py
+++ 11-2.py
@@ -7,37 +7,65 @@ def selecth(arr,k,lim,start,end):
     if end>len(arr):
         end = len(arr)
     median = MoM(arr,lim,start,end)
-    x = start+1
-    s = start+1
-    l = end-1
-    while x<end and s<l:
-        if arr[x]<median[0]:
-            arr[s],arr[x]=arr[x],arr[s]
-            s+=1
-        if arr[x]>median[0]:
-            arr[l],arr[x]=arr[x],arr[l]
-            l-=1
-            x-=1
-        x+=1
-    if arr[s]<median[0]:
-        arr[start],arr[s]=arr[s],arr[start]
-        median = (median[0],s) #medpt is index of array where the median is
-    else:
-        arr[start],arr[s-1]=arr[s-1],arr[start]
-        median = (median[0],s-1)
+    if end-start>=2:
+        x = start+1
+        s = start+1
+        l = end-1
+        # print(median)
+        while x<end and s<l:
+            if arr[x]<median[0]:
+                arr[s],arr[x]=arr[x],arr[s]
+                s+=1
+            if arr[x]>median[0]:
+                arr[l],arr[x]=arr[x],arr[l]
+                l-=1
+                x-=1
+            x+=1
+        if arr[s]>median[0]:
+            try:
+                if arr[s-1]<median[0]:
+                    arr[median[1]],arr[s-1]=arr[s-1],arr[median[1]]
+                    median=(median[0],s-1)
+                else:
+                    arr[median[1]],arr[s-2]=arr[s-2],arr[median[1]]
+                    median=(median[0],s-2)
+            except IndexError:
+                pass
+        else:
+            try:
+                if arr[s+1]>median[0]:
+                    arr[median[1]],arr[s]=arr[s],arr[median[1]]
+                    median=(median[0],s)
+                else:
+                    arr[median[1]],arr[s+1]=arr[s+1],arr[median[1]]
+                    median=(median[0],s+1)
+            except IndexError:
+                pass
+        # if arr[s]<median[0] and arr[s+1]>median[0]:
+        #     arr[start],arr[s]=arr[s],arr[start]
+        #     median = (median[0],s) #medpt is index of array where the median is
+        # else:
+        #     if arr[s-1]<median[0] and arr[s]>median[0]:
+        #         arr[start],arr[s-1]=arr[s-1],arr[start]
+        #         median = (median[0],s-1)
     if k==median[1]:
+        # print('FINISHED')
+        # print(median)
+        # print(arr)
+        # print(k)
         return median[0]
     else:
-        print("median after moving stuff" + str(median))
-        print(arr)
-        print('k: ' + str(k))
-        print('median: ' + str(median))
+        # print("median after moving stuff" + str(median))
+        # print(arr)
+        # print('k: ' + str(k))
+        # print('median: ' + str(median))
         if k<median[1]:
-            print("s")
+            # print("s")
             return selecth(arr,k,lim,start,median[1])
         else:
-            print('l')
-            return selecth(arr,k-median[1]-1,lim,median[1]+1,end)
+            # print('l')
+            # print((median[1]+1,end))
+            return selecth(arr,k,lim,median[1]+1,end)
 
 def s(arr,start,end): #does not include end
     if end>len(arr):
@@ -51,19 +79,26 @@ def s(arr,start,end): #does not include end
     return arr
 
 def MoM(arr,lim,start,end):
+    if end-start<2:
+        return (arr[start],start)
     numparts = math.ceil((end-start) / lim)
     for begin in range(numparts):
         s(arr,start+(begin*lim),start+(begin*lim)+lim)
     for x in range(numparts):
-        arr[start+x],arr[start+(x*lim)+(lim//2)]=arr[start+(x*lim)+(lim//2)],arr[start+x]
+        # print(numparts)
+        # print("ERROR: " + str((start,end)) + ' '+ str(x) + ' ' + str(start+(x*lim)+(lim//2)))
+        if start+(x*lim)+(lim//2)>=end:
+            arr[start+x],arr[start+(x*lim)+((end-(start+(x*lim)))//2)]=arr[start+(x*lim)+((end-(start+(x*lim)))//2)],arr[start+x]
+        else:
+            arr[start+x],arr[start+(x*lim)+(lim//2)]=arr[start+(x*lim)+(lim//2)],arr[start+x]
     return getMedian(arr,start,start+numparts)
 
 def getMedian(arr,start,end): #Does not look at arr[end]
-    print("start,end " + str((start,end)))
-    print("from this: " +str(arr))
+    # print("start,end " + str((start,end)))
+    # print("from this: " +str(arr))
     if end-start-1<=2:
-        print('hello')
-        print(str((arr[start],start)))
+        # print('hello')
+        # print(str((arr[start],start)))
         return (arr[start],start)
     else:
         for x in range(end-start):
@@ -74,15 +109,16 @@ def getMedian(arr,start,end): #Does not look at arr[end]
     return getMedian(arr,start+1,end-1)
 
 ###################################################################################################
-f = list(range(10))
-random.shuffle(f)
-print("original: " + str(f))
-# print(s(felicity,2,20))
-# print(f)
-print([select(f,i) for i in range(len(f))])
-# print(s(f,0,100))
+# f = list(range(222))
+# random.shuffle(f)
+# print("original: " + str(f))
+# # print(s(felicity,2,20))
+# # print(f)
+# print([select(f,i) for i in range(len(f))])
+# # print(s(f,0,100))
 
-# A=list(range(222))
-# random.shuffle(A)
-# print(select(A,5))
-# print([select(A,i) for i in range(220)])
+
+A=list(range(222))
+random.shuffle(A)
+print(select(A,5))
+print([select(A,i) for i in range(220)])
